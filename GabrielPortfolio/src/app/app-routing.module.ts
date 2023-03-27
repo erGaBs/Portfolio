5a577020f6f44eb1f82678f1bff5b83bcf2c64ea
GaBs <colla.gabriel01@gmail.com>
2023-03-27 16:54:12 +0200

    Aggiunta footer/header e componenti di routing

--- GabrielPortfolio/src/app/app-routing.module.ts
+++ GabrielPortfolio/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/main/home/home.component';
+import { AboutmeComponent } from './components/main/aboutme/aboutme.component';
+import { ProgettiComponent } from './components/main/progetti/progetti.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'aboutme', component: AboutmeComponent },
+  { path: 'progetti', component: ProgettiComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GabrielPortfolio/src/app/app.module.ts
+++ GabrielPortfolio/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/main/header/header.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import { HomeComponent } from './components/main/home/home.component';
+import { AboutmeComponent } from './components/main/aboutme/aboutme.component';
+import { ProgettiComponent } from './components/main/progetti/progetti.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutmeComponent,
+    ProgettiComponent
   ],
   imports: [
     BrowserModule,
