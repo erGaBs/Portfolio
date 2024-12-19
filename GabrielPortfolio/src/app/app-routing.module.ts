608c917e64660c0f2d09b30536abec876fe07fcf
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2024-12-20 00:08:56 +0100

    Rebuilding Totale

--- GabrielPortfolio/src/app/app-routing.module.ts
+++ GabrielPortfolio/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/main/home/home.component';
-import { AboutmeComponent } from './components/main/aboutme/aboutme.component';
-import { ProgettiComponent } from './components/main/progetti/progetti.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'aboutme', component: AboutmeComponent },
-  { path: 'progetti', component: ProgettiComponent }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- GabrielPortfolio/src/app/app.module.ts
+++ GabrielPortfolio/src/app/app.module.ts
@@ -3,42 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/main/header/header.component';
-import { FooterComponent } from './components/main/footer/footer.component';
-import { HomeComponent } from './components/main/home/home.component';
-import { AboutmeComponent } from './components/main/aboutme/aboutme.component';
-import { ProgettiComponent } from './components/main/progetti/progetti.component';
-import { CarouselComponent } from './components/utils/carousel/carousel.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ChiSonoComponent } from './components/utils/chi-sono/chi-sono.component';
-import { ContattiComponent } from './components/utils/contatti/contatti.component';
-import { SoftSkillsComponent } from './components/utils/soft-skills/soft-skills.component';
-import { HardSkillsComponent } from './components/utils/hard-skills/hard-skills.component';
-import { ToastModule } from 'primeng/toast';
-import { MessageService } from 'primeng/api';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    AboutmeComponent,
-    ProgettiComponent,
-    CarouselComponent,
-    ChiSonoComponent,
-    ContattiComponent,
-    SoftSkillsComponent,
-    HardSkillsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ToastModule
+    AppRoutingModule
   ],
-  providers: [MessageService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
