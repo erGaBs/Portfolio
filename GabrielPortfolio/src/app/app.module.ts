3091b91978684043313a993fe8cc22d700768974
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2024-12-20 01:23:01 +0100

    primi componenti riorganizzati

--- GabrielPortfolio/src/app/app.module.ts
+++ GabrielPortfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/main/dashboard/dashboard.component';
+import { HeaderComponent } from './components/main/header/header.component';
+import { FooterComponent } from './components/main/footer/footer.component';
+import { ProjectsComponent } from './components/sections/projects/projects.component';
+import { PresentationComponent } from './components/sections/presentation/presentation.component';
+import { SkillsComponent } from './components/sections/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProjectsComponent,
+    PresentationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
