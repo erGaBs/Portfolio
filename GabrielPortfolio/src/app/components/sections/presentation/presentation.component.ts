9173294d3b114365856bbd0a488920ed6eed4105
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2025-06-17 23:56:14 +0200

    fine refactor grafico
    (manca aggiunta cambio linguaggio)

--- GabrielPortfolio/src/app/components/sections/presentation/presentation.component.ts
+++ GabrielPortfolio/src/app/components/sections/presentation/presentation.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-presentation',
   templateUrl: './presentation.component.html',
-  styleUrls: ['./presentation.component.css']
+  styleUrls: ['./presentation.component.scss']
 })
 export class PresentationComponent {
+animate = true;
 
+  constructor() {
+    setInterval(() => {
+      this.animate = false;
+      setTimeout(() => this.animate = true, 50);
+    }, 8000);
+  }
 }
