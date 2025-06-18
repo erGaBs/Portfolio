9603899efd32204da9be4171fa5522fe68b614a1
Gabriel Colla <101674782+erGaBs@users.noreply.github.com>
2025-06-18 14:47:37 +0200

    rimozione cose inutili

--- GabrielPortfolio/src/app/app.component.ts
+++ GabrielPortfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GabrielPortfolio';
+  constructor(private titleService: Title, private metaService: Meta) { }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('Gabriel Colla - Software Engineer');
+
+    this.metaService.addTags([
+      { name: 'description', content: 'Portfolio di Gabriel Colla, Software Engineer specializzato in sviluppo web, applicazioni Angular, e soluzioni software innovative.' },
+      { name: 'keywords', content: 'Gabriel Colla, software engineer, sviluppo software, Angular developer, programmatore, sviluppo web, tecnologia, coding, programmazione' },
+      { name: 'author', content: 'Gabriel Colla' },
+      { name: 'robots', content: 'index, follow' }
+    ]);
+  }
 }
